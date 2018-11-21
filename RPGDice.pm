08c4f48dc6d5fd0ee3ca3ed0049641cfa0a27230
SakauranoKuriko <mao.liz.baka@gmail.com>
2018-11-21 15:32:30 +0800

    创建项目

--- RPGDice.pm
+++ RPGDice.pm
@@ -9,30 +9,12 @@ sub call{
         return if not $msg->allow_plugin;
         return if $msg->content !~ /^\.r/;
         my r = int rr(1,20);
+        #my $key_word = $1;$key_word=~s/\s+//;
+        #my $reply = $reply[int rand($#reply+1)];
+        #$reply=~s/%/$key_word/g;
+        #$client->reply_message($msg,$reply,sub{$_[1]->from("bot")}) if $reply;
         $client->reply_message($msg, "hello world");
         $msg->allow_plugin(0);
-    });
-}
-1;
-package Mojo::Webqq::Plugin::FuckDaShen;
-
-my @reply = (
-    "动不动就叫%...",
-    "%能不能换点别的称呼...",
-    "%，%，%，%..喜欢你就叫个够...",
-    "请问%是指哪位?",
-    "能不能别随随便便就叫%?"
-);
-sub call {
-    my $client = shift;
-    $client->on(receive_message=>sub{
-        my($client,$msg)=@_;
-        return if not $msg->allow_plugin;
-        return if $msg->content !~ /(大\s*神|大\s*婶|大\s*侠)/;
-        my $key_word = $1;$key_word=~s/\s+//;
-        my $reply = $reply[int rand($#reply+1)];
-        $reply=~s/%/$key_word/g;
-        $client->reply_message($msg,$reply,sub{$_[1]->from("bot")}) if $reply;
     });
 }
 1;
